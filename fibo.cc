5abc36d0f4e9aa4581d338fa8f0ca82ac3eba3b3
niedziol <michalekniedziolka@gmail.com>
2019-11-20 22:12:50 +0100

    Add normalize

--- fibo.cc
+++ fibo.cc
@@ -8,7 +8,17 @@ Fibo::Fibo() {
 }
 
 void Fibo::normalize() {
-    
+    for (unsigned i = bits.size() - 1; i >= 1; --i) {
+        if (bits[i] & bits[i - 1]) {
+            if (i == bits.size() - 1) {
+                bits.push_back(1);
+            } else {
+                bits[i + 1] = 1;
+                bits[i] = 0;
+                bits[i] = 0;
+            }
+        }
+    }
 }
 
 const Fibo operator+(const Fibo& a, const Fibo& b) {
